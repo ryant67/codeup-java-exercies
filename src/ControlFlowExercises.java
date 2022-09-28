57b75af9c86e500a3429a302ef448bccf3fb98f4
Ryan Harris <ryan.t.harris67@gmail.com>
2022-09-28 16:47:26 -0500

    exercise updated

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,47 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for(int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
-        }
+////////////////////////////////////////////////////////Exercise 1A: While Loop
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+////////////////////////////////////////////////////////Exercise 1A: For Loop
+//        for(int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+////////////////////////////////////////////////////////Exercise 1B(1): Do/While Loop
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+////////////////////////////////////////////////////////Exercise 1B(2): Do/While Loop
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+////////////////////////////////////////////////////////Exercise 1B(3): Do/While Loop
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i < 1000000);
+////////////////////////////////////////////////////////Exercise 1B(1): For Loop
+//        for(int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+////////////////////////////////////////////////////////Exercise 1B(2): For Loop
+//        for(int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+////////////////////////////////////////////////////////Exercise 1B(3): For Loop
+//        for(int i = 2; i < 1000000; i += 2) {
+//            System.out.println(i);
+//        }
+////////////////////////////////////////////////////////Exercise 2: FizzBuzz
 
 
 
