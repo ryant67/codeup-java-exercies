14e77b021e9adc860e081443a5ef20bcd702d5f1
Ryan Harris <ryan.t.harris67@gmail.com>
2022-09-28 15:52:47 -0500

    ControlFlow exerc 1 complete

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        for(int i = 5; i <= 15; i++) {
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
