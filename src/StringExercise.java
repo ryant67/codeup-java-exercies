64e3fd8040a481fc3ee2223a7ef91e3819f7508f
Ryan Harris <ryan.t.harris67@gmail.com>
2022-09-29 11:26:37 -0500

    String Exercise complete

---
+++ src/StringExercise.java
@@ -0,0 +1,38 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+//        We don't need no education
+//        We don't need no thought control
+
+        String firstOutput = "We don't need no education\nWe don't need no thought control";
+        System.out.println(firstOutput);
+
+//        Check "this" out!, "s inside of "s!
+
+        String secondOutput = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(secondOutput);
+
+//        In windows, the main drive is usually C:\
+
+        String thirdOutput = "In windows, the main drive is usually C:\\";
+        System.out.println(thirdOutput);
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+        String fourthOutput = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(fourthOutput);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
