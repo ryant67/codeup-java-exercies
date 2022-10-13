015e5caee85bb20722df2b1f5374a39739179f4b
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-13 15:45:09 -0500

    updated

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -10,67 +10,57 @@ public class GradesApplication {
 
         HashMap<String, Student> students = new HashMap<>();
         //new Student Instances
-        Student student0 = new Student("George");
-        Student student1 = new Student("Joey");
-        Student student2 = new Student("Jane");
-        Student student3 = new Student("Lucy");
-        //student0 grades
-        student0.addGrade(90);
-        student0.addGrade(80);
-        student0.addGrade(85);
-        //student1 grades
-        student1.addGrade(95);
-        student1.addGrade(100);
-        student1.addGrade(89);
-        //student2 grades
-        student2.addGrade(95);
-        student2.addGrade(100);
-        student2.addGrade(89);
-        //student3 grades
-        student3.addGrade(95);
-        student3.addGrade(100);
-        student3.addGrade(89);
-        //Adding students to HashMap
-        students.put("GeorgyIT", student0);
-        students.put("Joey123", student1);
-        students.put("Jane321", student2);
-        students.put("Lucyloohoo", student3);
+        Student george = new Student("George");
+        george.addGrade(90);
+        george.addGrade(80);
+        george.addGrade(70);
+        students.put("george1", george);
+
+        Student joey = new Student("Joey");
+        joey.addGrade(95);
+        joey.addGrade(85);
+        joey.addGrade(75);
+        students.put("joey2", joey);
+
+        Student jane = new Student("Jane");
+        jane.addGrade(99);
+        jane.addGrade(89);
+        jane.addGrade(79);
+        students.put("jane3", jane);
+
+        Student lucy = new Student("Lucy");
+        lucy.addGrade(98);
+        lucy.addGrade(88);
+        lucy.addGrade(78);
+        students.put("lucy4", lucy);
 
         //GitHub username creator
         ArrayList<String> names = new ArrayList<>();
         for(String keys : students.keySet()) {
             names.add("|" + keys + "|");
         }
-        System.out.println(names);
+//        System.out.println(names);
 
         String newName = "";
         for(int i = 0; i < names.size(); i++) {
             newName += names.get(i) + " ";
         }
-        System.out.println(newName);
-
-//        String newName = "";
-//        String nameFormat = "";
-//        for(int i = 0; i < names.size(); i++) {
-//            newName += names.get(i) + " ";
-//            nameFormat += "|" + names.get(i) + "| ";
-//        }
 //        System.out.println(newName);
 
         //Command Line Interface
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Hello!");
-//        System.out.println("");
-//        System.out.println("Here are the GitHub usernames for our students: ");
-//        System.out.println("");
-//        System.out.println(nameFormat);
-//        System.out.println("");
-//        System.out.println("Which student would you like to see more information on?");
-//        String userInput = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello!");
+        System.out.println("");
+        System.out.println("Here are the GitHub usernames for our students: ");
+        System.out.println("");
+        System.out.println(newName);
+        System.out.println("");
+        System.out.println("Which student would you like to see more information on?");
+        String userInput = scan.nextLine();
 //        System.out.println(userInput);
-//
+
 //        if(userInput.equalsIgnoreCase(newName)) {
-//            System.out.println("Name: " + student0.getName() + " - GitHub UserName: " + names + "%nCurrent Average: " + student0.getGradeAverage());
+//            System.out.println("Name: " + .getName() + " - GitHub UserName: " + names + "Current Average: " + .getGradeAverage());
 //        } else {
 //            System.out.println("Sorry, no student was found with the GitHub username of " + "\"" + userInput + "\"");
 //        }
